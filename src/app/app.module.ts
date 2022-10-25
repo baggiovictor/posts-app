f1c417bb2ef93d7384d9b6cfb28525405d5584e9
Victor Baggio <baggiobaggiovictor@gmail.com>
2022-10-25 17:29:16 -0300

    finalizando a aula

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -5,7 +5,7 @@ import { AppComponent } from './app.component';
 import { CabecalhoComponent } from './components/cabecalho/cabecalho.component';
 import { RodapeComponent } from './components/rodape/rodape.component';
 import { CriarPensamentosComponent } from './components/pensamentos/criar-pensamentos/criar-pensamentos.component';
-import { FormsModule } from "@angular/forms";
+import {FormsModule, ReactiveFormsModule} from "@angular/forms";
 import { ListarPensamentoComponent } from './components/pensamentos/listar-pensamento/listar-pensamento.component';
 import { AppRoutingModule } from "./app-routing.module";
 import { PensamentoComponent } from './components/pensamentos/pensamento/pensamento.component';
@@ -28,7 +28,8 @@ import {EditarPensamentoComponent} from "./components/pensamentos/editar-pensame
     BrowserModule,
     FormsModule,
     AppRoutingModule,
-    HttpClientModule
+    HttpClientModule,
+    ReactiveFormsModule
   ],
   providers: [],
   bootstrap: [AppComponent]
--- src/app/components/pensamentos/criar-pensamentos/criar-pensamentos.component.ts
+++ src/app/components/pensamentos/criar-pensamentos/criar-pensamentos.component.ts
@@ -1,31 +1,41 @@
-import {Component} from '@angular/core';
+import {Component, OnInit} from '@angular/core';
 import {Pensamento} from "../pensamento";
 import {PensamentoService} from "../pensamento.service";
 import {Router} from "@angular/router";
+import {FormBuilder, FormGroup, Validators} from "@angular/forms";
 
 @Component({
   selector: 'app-criar-pensamentos',
   templateUrl: './criar-pensamentos.component.html',
   styleUrls: ['./criar-pensamentos.component.css']
 })
-export class CriarPensamentosComponent {
+export class CriarPensamentosComponent implements OnInit{
 
-  pensamento: Pensamento = {
-    conteudo: '',
-    autoria: '',
-    modelo: 'modelo1'
+  form!: FormGroup;
+
+  constructor(private service: PensamentoService, private router: Router, private formBuilder: FormBuilder) { }
+
+  ngOnInit(): void {
+    this.form = this.formBuilder.group({
+      conteudo: ['', Validators.compose([Validators.required, Validators.pattern(/(.|\s)*\S(.|\s)*/)])],
+      autoria: ['', Validators.compose([Validators.required, Validators.minLength(3)])],
+      modelo: ['modelo1']
+    })
   }
 
-  constructor(private service: PensamentoService, private router: Router) { }
-
   criarPensamento() {
-    this.service.criar(this.pensamento)
-      .subscribe(()=> this.router.navigate(['/listar-pensamento']))
+    console.log(this.form.get('autoria')?.errors)
+    if (this.form.valid) {
+      this.service.criar(this.form.value)
+        .subscribe(()=> this.router.navigate(['/listar-pensamento']))
+    }
   }
 
   cancelar() {
     this.router.navigate(['/listar-pensamento']);
   }
 
-
+  habilitarBotao(): string {
+    return this.form.valid ? 'botao' : 'botao__desabilitado'
+  }
 }
